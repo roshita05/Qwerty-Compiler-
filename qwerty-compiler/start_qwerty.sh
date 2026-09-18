e5b03f108cdc4ddf19383b53aa794ced92fc4c7d
Roshita Ailawadi <115929185+roshita05@users.noreply.github.com>
2026-09-18 15:40:48 +0530

    Delete qwerty-compiler directory

--- qwerty-compiler/start_qwerty.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -eu
-cd "$(dirname "$0")"
-python3 -c 'import sys; raise SystemExit(0 if sys.version_info >= (3, 11) else "Python 3.11 or newer is required.")'
-exec python3 -m qwerty web "$@"
